205d408738ef0f37bbefa31af8259016a339ca30
A0X0E0L <ax13xxi@gmail.com>
2023-01-19 20:56:24 +0000

    Reporte final, Todo´s con fetch, doy refresh y aún así se guardan las tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,111 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
+
+	function createUser(){ //creo el usuario se usa una vez
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/Axminator`,
+		{method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify([])
+	})
+	.then((response)=>response.json())
+	.then((response)=>console.log(response))
+	}
+	
+	useEffect(() =>{  // Primero creo el usuario y luego lo uso para traer la lista
+		// createUser()
+		getLista()
+	}, [])
+
+
+	function transporteDatos(){   //Guardo la lista en el usuario
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/Axminator`,
+		{method: 'PUT',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(listaTareas)
+	  })
+		.then((response)=>response.json())
+		.then((response)=>console.log(response))
+	}
+	
+useEffect(() => {    //Modifico la lista
+    transporteDatos()
+},listaTareas)
+
+
+function getLista(){                    //Traigo la lista modificada del usuario
+    fetch(`https://assets.breatheco.de/apis/fake/todos/user/Axminator`,
+	{method: 'GET'
+  })
+	.then((response)=>response.json())
+	.then((response)=>setlistaTareas(response))
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	const[tarea,setTarea]=useState("")
+	const[listaTareas,setlistaTareas]=useState([])
+
+	
+
+	const handleSubmit	= (e) => {
+	
+	
+	
+	
+		e.preventDefault()
+		setTarea("")
+
+		setlistaTareas([...listaTareas, { "label": tarea, "done": false }]);
+
+		
+	}
+	console.log(listaTareas);
+
+
+	const Borrar = (posicion)=>{
+setlistaTareas(listaTareas.filter((item,index) => index !== posicion));
+
+	}
+
+
+
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+		<>
+
+		<h1 className="d-flex justify-content-center" >To Do List</h1>
+		<form className="container" onSubmit={handleSubmit}>
+  <div className="mb-3">
+    
+    {/*2. definimos el evento ochange en el input */}
+	<input type="text" className="form-control" label="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e)=>{setTarea(e.target.value)}}/>
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Submit</button>
+{/* item sería cualquier dato, puede ser index también */}
+<ul>
+	
+	{listaTareas.map((item,index) => (<li key={index}> {item.label}<button type="button" className="btn btn-outline-light" onClick={(()=>Borrar(index))} >X</button></li>))}
+	
+</ul>
+
+
+
+</form>
+
+<div>
+	
+</div>
+</>
 	);
 };
 
